84e0312e1ba9d7818c1975e7463420cd221d626a
jeremy jestel <jjeste8@wgu.edu>
2024-06-16 20:59:28 -0400

    show the accuracy of the model

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import classification_report, roc_auc_score, accuracy_score, roc_curve, auc
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_auc_score
 from imblearn.under_sampling import RandomUnderSampler
-from sklearn.metrics import roc_curve, auc
+from sklearn.metrics import roc_curve, auc, accuracy_score
+import numpy as np
 data = pd.read_csv('clean_dataset.csv')
 
 X = data.drop('Approved', axis=1)
@@ -22,18 +20,5 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-y_pred_proba = model.predict_proba(X_test)[:,1]
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
-roc_auc = auc(fpr, tpr)
-
-plt.figure()
-plt.plot(fpr, tpr, color='blue', lw=2, label=f'ROC Curve (AUC = {roc_auc:.2f})')
-plt.plot([0, 1], [0, 1], color='red', linestyle='--')
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc='lower right')
-plt.show()
-
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
